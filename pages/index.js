4875351a31dba8bf8b3cbd03a87393af740e9b4c
changxinxing <rissatk08@gmail.com>
2021-05-06 17:31:17 +0800

    mongo connect

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home({ isConnected }) {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>MongoDB with Next</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
